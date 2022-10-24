45a4be6f34bd52a39604d6f12b7a278c81fbfc78
Lucas Daniel <ldcs@cin.ufpe.br>
2022-10-23 23:32:12 -0300

    <feat> all querys done

--- Database/gdi/dml_consult.sql
+++ Database/gdi/dml_consult.sql
@@ -93,12 +93,97 @@ order by ESTADO
 -- NOME DE TODOS OS SUPERVISORES E QUANTIDADE DE MEDICOS QUE SUPERVISIONAM MAIS DE 3 MÉDICOS (USANDO GROUP BY E INNER JOIN)
 
 SELECT NOME, count(SUPERVISOR) as qtd FROM FUNCIONARIO 
-inner join MEDICOS on FUNCIONARIO.CPF = MEDICOS.SUPERVISOR
+INNER JOIN MEDICOS on FUNCIONARIO.CPF = MEDICOS.SUPERVISOR
 group by nome, Supervisor
 HAVING COUNT(SUPERVISOR) > 3
 order by QTD DESC
 
 -- NOME DE TODOS OS PACIENTES QUE NÃO SÃO FUNCIONARIOS (ANTI-JOIN COM NOT EXISTS)
 SELECT nome FROM paciente p
-WHERE not exists (SELECT f.cpf FROM funcionario f
+WHERE NOT EXISTS (SELECT f.cpf FROM funcionario f
                 WHERE p.cpf = f.cpf)
+
+-- NOME E CPF DE MÉDICOS QUE POSSUEM MAIS DE UMA CONSULTA (INNER-JOIN, GROUP BY E HAVING)
+SELECT F.NOME, A.CPF_MEDICO, COUNT(*) AS QTD 
+FROM AGENDAMENTO A
+INNER JOIN FUNCIONARIO F ON (F.CPF = A.CPF_MEDICO)
+GROUP BY F.NOME, A.CPF_MEDICO
+HAVING COUNT(*) > 1;
+
+--NOME E CPF DE MÉDICOS QUE POSSUEM MAIS DE UMA CONSULTA (INNER-JOIN, INNER-JOIN, GROUP BY E HAVING)
+select f.nome, m.cpf, count(*) from medicos m
+inner join funcionario f on f.cpf = m.cpf 
+inner join agendamento on m.cpf = cpf_medico
+group by f.nome, m.cpf
+having count(*)>1
+
+--TODOS OS FUNCIONARIOS E SEUS BENEFICIOS, CASO RECEBAM (FULL OUTER JOIN)
+SELECT B.COD, F.CPF, F.NOME
+FROM BENEFICIO B
+FULL OUTER JOIN FUNCIONARIO F ON (B.CPF = F.CPF);
+
+--CPF DOS MÉDICOS QUE PARTICIPAM DE UM AGENDAMENTO 
+SELECT cpf
+FROM Funcionario
+UNION
+SELECT cpf_medico
+FROM agendamento
+
+--FUNCIONARIOS QUE SÃO PAGOS E NÃO RECEBEM BENEFÍCIOS
+SELECT count(cpf) FROM paga_funcionario p
+WHERE NOT EXISTS (SELECT cpf FROM beneficio b
+                WHERE p.cpf = b.cpf);
+
+--FUNCIONARIOS QUE SÃO PAGOS E NÃO RECEBEM BENEFÍCIOS
+SELECT count(p.cpf) FROM paga_funcionario p
+LEFT JOIN beneficio b on b.cpf = p.cpf
+WHERE b.cpf is null;
+
+
+--NOME E CONTATO DAS EMPRESAS QUE OFERECEM O MESMO SERVIÇO QUE A VIVAMUS INSTITUTE ORDENADO ALFABETICAMENTE (SUBCONSULTA TIPO LINHA)
+SELECT E.RAZAO_SOCIAL, E.CONTATO
+FROM EMPRESA_TERCEIRIZADA E
+WHERE FUNCAO = (SELECT FUNCAO
+                FROM EMPRESA_TERCEIRIZADA
+                WHERE RAZAO_SOCIAL = 'Vivamus Institute')
+ORDER BY E.RAZAO_SOCIAL ASC;
+
+--CRM DE FUNCIONARIOS COM EMAIL DA UFPE (SUBCONSULTA TIPO TABELA)
+select m.crm from funcionario f
+inner join medicos m on m.cpf = f.cpf
+where f.cpf in (select cpf from email_funcionario
+            where email like '%@ufpe.br')
+
+--FUNCIONARIOS QUE TRABALHAM COM A CLINICA DE ID 8 (INNER JOIN)
+select f.nome, r.cpf from recepcionista r
+inner join funcionario f on f.cpf = r.cpf 
+where descricao = 'alergia'
+
+--EMPRESA TERCEIRIZADA QUE ESTAO TENDO SERVICOS SENDO REALIZADOS (SEMI-JOIN, SUBCONSULTA TIPO TABELA)
+select cnpj, razao_social
+from empresa_terceirizada
+where exists(select 1
+from servico
+where servico.cnpj = empresa_terceirizada.cnpj);
+
+-- CLINICAS QUE NAO ESTAO TENDO SERVICOS SENDO REALIZADOS (ANTI-JOIN, SUBCONSULTA TIPO TABELA)
+select ID, razao_social
+from clinica
+where not exists(select 1
+from servico
+where servico.id_clinica = clinica.ID);
+
+
+--CONTRATOS COM VALOR MAIOR QUE 5 MIL (SEMI-JOIN, SUBCONSULTA TIPO TABELA)
+select cod, valor
+from contrato
+where exists(select 1
+from servico
+where contrato.cod = servico.cod and valor > 5000)
+
+--PACIENTES QUE MORAM NA MESMA CIDADE QUE A CLINICA (OPERACAO DE CONJUNTO, INTERSECT)
+select cidade
+from paciente
+intersect
+select cidade
+from CLINICA
