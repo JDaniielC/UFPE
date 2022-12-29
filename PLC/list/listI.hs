a6987b9eaeb13102e1950381e075156c804e7070
José Daniel <jdsc2@cin.ufpe.br>
2022-12-29 14:57:24 -0300

    CHORE: Add new solution and change names

---
+++ PLC/list/listI.hs
@@ -0,0 +1,11 @@
+main = do
+       a <- getLine
+       let result = bSort (read a :: [String])
+       print result
+
+bSort :: [String] -> [String]
+bSort [] = []
+bSort (x: xs) =
+      bSort [y | y <- xs, y < x] ++
+      [x] ++
+      bSort [y | y <- xs, y >= x]
